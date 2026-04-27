619fc9309b132270828da313829cef1ac69443b1
Uday <udaykiran136.uk@gmail.com>
2026-04-27 21:19:20 +0530

    "FixRoutingSyntax"

--- campus-lost-and-found/server/index.js
+++ campus-lost-and-found/server/index.js
@@ -182,7 +182,7 @@ app.post('/api/reset-password', async (req, res) => {
 });
 
 // Handle any requests that don't match the API routes (Routing fallback)
-app.get('(.*)', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
